09e05aa2a2d870eb24c288f17f9a96e04b0b8c5b
Andrew <skat2006@bk.ru>
2022-09-16 16:15:00 +0300

    Initial commit

---
+++ src/main/java/ru/spb/curs2/Curs2Application.java
@@ -0,0 +1,13 @@
+package ru.spb.curs2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Curs2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Curs2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/spb/curs2/Curs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spb.curs2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Curs2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
