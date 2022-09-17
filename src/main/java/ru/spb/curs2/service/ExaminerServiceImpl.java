4c81842f1108634dee015f218bf370e7f02b0b13
Andrew <skat2006@bk.ru>
2022-09-17 12:02:28 +0300

    load4

--- src/main/java/ru/spb/curs2/service/ExaminerServiceImpl.java
+++ src/main/java/ru/spb/curs2/service/ExaminerServiceImpl.java
@@ -2,6 +2,7 @@ package ru.spb.curs2.service;
 
 import org.springframework.stereotype.Service;
 import ru.spb.curs2.exception.LackOfQuestionsException;
+import ru.spb.curs2.exception.WrongQuestionsAmountException;
 import ru.spb.curs2.questionary.Question;
 import java.util.Collection;
 import java.util.HashSet;
@@ -17,6 +18,8 @@ public class ExaminerServiceImpl implements ExaminerService {
 
     @Override
     public Collection<Question> getQuestions(int amount) throws LackOfQuestionsException {
+        if (amount < 1) throw new WrongQuestionsAmountException();
+
         Set<Question> questionList = new HashSet<>();
         if (questionService.getSize() >= amount) {
             while (questionList.size() < amount) {
